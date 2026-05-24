8c5d3fab3eb93a57f56178555a0c77f9f74a5007
Himanshu-07-Sunariya <himanshu.sunariya24@gmail.com>
2026-05-25 01:12:46 +0530

    App basic layout is done

---
+++ Frontend/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        Today's Medicines
+
+        No medicines added
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        Medicine Reminder App
+        Dashboard
+        Add Medicine
+        Reminders
+    </div>
+  )
+}
+
+export default Navbar
